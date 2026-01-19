2d9f1350a501c698fec599fd69251fdef29fc045
MGX <3034181677@qq.com>
2026-01-19 09:26:40 +0800

    测试

---
+++ 66.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{   int a=1555;
+    printf("%d",a);
+    printf("hfsadljfhfjh");
+    
+    return 0;
+}
---
+++ Untitled-1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+    printf("你好世界");
+    
+    return 0;
+}
